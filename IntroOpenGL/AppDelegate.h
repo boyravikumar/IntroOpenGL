890f88ab67c5a6f62fff578de20971999b73ab10
boyapati ravi kumar <boyravikumar@gmail.com>
2015-06-30 10:24:08 +0530

    Initial Commit

---
+++ IntroOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroOpenGL
+//
+//  Created by boyapati ravi kumar on 30/06/15.
+//  Copyright (c) 2015 Custom Furniish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntroOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntroOpenGL
+//
+//  Created by boyapati ravi kumar on 30/06/15.
+//  Copyright (c) 2015 Custom Furniish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
